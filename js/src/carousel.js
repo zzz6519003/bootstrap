f073d140512324235f763911c89491336c3aa91d
GeoSot <geo.sotis@gmail.com>
2021-09-10 02:55:28 +0300

    config merge is redundant, because it is done by default

--- js/src/carousel.js
+++ js/src/carousel.js
@@ -480,7 +480,6 @@ class Carousel extends BaseComponent {
     }
 
     const config = {
-      ...Manipulator.getDataAttributes(target),
       ...Manipulator.getDataAttributes(this)
     }
     const slideIndex = Manipulator.getDataAttribute(this, 'slide-to')
